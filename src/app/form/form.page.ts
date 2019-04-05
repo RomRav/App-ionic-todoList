763431f9a970baa8b529648d31bd12674fdb110b
RORO <RORO@jgmail.com>
2019-04-05 15:27:04 +0200

    Add Picture

--- src/app/form/form.page.ts
+++ src/app/form/form.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ɵConsole } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Storage } from '@ionic/storage';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-form',
@@ -11,7 +12,9 @@ import { Storage } from '@ionic/storage';
 export class FormPage implements OnInit {
   public todoAdd;
   private pos;
-  //
+  public pictureData;
+
+
   constructor(
     private todoService: TodoService,
     //Injection de la class Router pour permettre un routage vers une autre page
@@ -19,7 +22,9 @@ export class FormPage implements OnInit {
     //Injection de la class Storage pour permettre la sauvegarde des données
     private storage: Storage,
     //Injection de la class ActivatedRoute pour permettre de récupérer la variable transmise via l'URL
-    private activeRoute: ActivatedRoute
+    private activeRoute: ActivatedRoute,
+    //Injection de la class Camera pour gérer l'appareil photo 
+    private camera: Camera
   ) { }
 
 
@@ -60,6 +65,30 @@ export class FormPage implements OnInit {
         this.router.navigateByUrl("/home");
       })
     };
+
+  }
+
+  takePicture() {
+    const cameraOptions: CameraOptions = {
+      //defini la qualité de la photo (60 = 60% de la qlté)
+      quality: 60,
+      //URI = universal resource identifieur
+      //URL = universal resource locator
+      destinationType: this.camera.DestinationType.FILE_URI,
+      //defini la hauteur et largeur de la photo
+      targetHeight: 500,
+      targetWidth: 500,
+      encodingType: this.camera.EncodingType.JPEG,
+      //Defini le type de fichier (photo / video)
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(cameraOptions).then((cameraData) => {
+      this.pictureData = cameraData;
+
+    }, (err) => {
+      console.log(err);
+    })
+
 
   }
 }
